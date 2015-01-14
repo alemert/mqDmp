ecef078974e192c3d75db333c8876eff94c5c161
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2015-01-14 16:18:43 +0000

    still working on qmgr alias, feierabend
    git-svn-id: http://syspver1/svn/develop/modules/mqDmp@2345 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- src/alias.c
+++ src/alias.c
@@ -40,6 +40,7 @@
 /******************************************************************************/
 /*   D E F I N E S                                                            */
 /******************************************************************************/
+#define EMPTY_Q_NAME "                                                 "
 
 /******************************************************************************/
 /*   M A C R O S                                                              */
@@ -72,6 +73,7 @@ const char** getQmgrAliases( MQHCONN hCon, int *sysRc )
   MQHBAG qAttrBag ;
 
   MQLONG lng ;
+  MQCHAR qname[MQ_Q_NAME_LENGTH];
   char** alias = NULL;
 
   // -------------------------------------------------------
@@ -92,16 +94,57 @@ const char** getQmgrAliases( MQHCONN hCon, int *sysRc )
   }
 
   // -------------------------------------------------------
-  // dis qr(*) queue
+  // setup PCF witch following MQSC:
+  // dis qr(*) xmitq rqmname where( rname ne '' )
   // -------------------------------------------------------
-  mqrc = mqAddStr( cmdBag, MQCA_Q_NAME, MQBL_NULL_TERMINATED, "*");
-  switch( mqrc )
-  {
-    case MQRC_NONE : break;
-    default: goto _door;
-  }
+  mqrc = mqAddStr( cmdBag              ,       //
+                   MQCA_Q_NAME         ,       //
+                   MQBL_NULL_TERMINATED,       // displaying remote queues needs
+                   "*"                );       //  two commands
+  switch( mqrc )                               //
+  {                                            // first: 
+    case MQRC_NONE : break;                    //    display queue(*)
+    default: goto _door;                       //
+  }                                            //
+                                               //
+  mqrc = mqAddInt( cmdBag       ,              // second:
+                   MQIA_Q_TYPE  ,              //    of type qremote
+                   MQQT_REMOTE );              //
+  switch( mqrc )                               //
+  {                                            //
+    case MQRC_NONE : break;                    //
+    default: goto _door;                       //
+  }                                            //
+                                               //
+  mqrc = mqSetInqAttr( cmdBag          ,       // additional attributes
+                       MQCA_XMIT_Q_NAME,       //   XMITQ
+                       MQCA_REMOTE_Q_MGR_NAME);//  RQMNAME
+  switch( mqrc )                               //
+  {                                            //
+    case MQRC_NONE : break;                    //
+    default: goto _door;                       //
+  }                                            //
+                                               //
+  mqrc = mqAddStrFilter( cmdBag            ,   // where condition
+                         MQCA_REMOTE_Q_NAME,   //  Remote Queue
+                         MQ_Q_NAME_LENGTH    , //    equals
+                         EMPTY_Q_NAME      ,   //  empty queue
+                         MQCFOP_EQUAL     );   //
+  switch( mqrc )                               //
+  {                                            //
+    case MQRC_NONE : break;                    //
+    default: goto _door;                       //
+  }                                            //
+                                               //
 
-  mqrc = mqAddInt( cmdBag, MQIA_Q_TYPE, MQQT_REMOTE );
+#if(0)
+  mqrc = mqAddStrFilter( cmdBag, 
+                         MQCA_XMIT_Q_NAME, 
+                         MQ_Q_NAME_LENGTH  , 
+			 "                                                 ",
+                         MQCFOP_NOT_EQUAL     );
+#endif
+
   switch( mqrc )
   {
     case MQRC_NONE : break;
@@ -160,21 +203,55 @@ const char** getQmgrAliases( MQHCONN hCon, int *sysRc )
     }                                     //
                                           //
     alias[i] = (char*)malloc(sizeof(char)*(itemCnt)*(MQ_Q_NAME_LENGTH+1));
-    if( alias[i] == NULL )                //
+    if( alias[i] == NULL )                // allocate memory for queue name
     {                                     //
       logger(LSTD_MEM_ALLOC_ERROR);       //
       mqrc = -1 ;                         //
-      goto _door;                         //
+      goto _door;                         // set the queue name on empty
     }                                     //
     memset(alias[i],(int)' ',MQ_Q_NAME_LENGTH); 
     alias[i][MQ_Q_NAME_LENGTH] = '\0';    //
                                           //
-    mqrc = mqStrInq( qAttrBag          ,  // attribute bag
-                     MQCA_Q_NAME       ,  // selector (queue name)
-                     0                 ,  // item index
-                     MQ_Q_NAME_LENGTH  ,  // available buffer length
-                     (PMQCHAR) alias[i],  // buffer
-                     &lng             );  // real buffer length
+    mqrc = mqStrInq( qAttrBag        ,    // check for XMITQ 
+                     MQCA_XMIT_Q_NAME,    // attribute in Remote Queue
+                     0               ,    // 
+                     MQ_Q_NAME_LENGTH,    // 
+                     qname           ,    // 
+                     &lng           );    // 
+    switch( mqrc )                        //
+    {                                     //
+      case MQRC_NONE : break;             //
+      default: goto _door;                //
+    }                                     //
+                                          //
+    if( memcmp( qname, EMPTY_Q_NAME, MQ_Q_NAME_LENGTH) == 0 )
+    {                                     // ignore all queues with 
+      continue;                           //   empty transmission
+    }                                     //
+                                          //
+    mqrc = mqStrInq( qAttrBag        ,    // check for RQMNAME
+                     MQCA_REMOTE_Q_NAME,  //  attribute in Remote Queue
+                     0               ,    // 
+                     MQ_Q_NAME_LENGTH,    // 
+                     qname           ,    // 
+                     &lng           );    // 
+    switch( mqrc )                        // 
+    {                                     //
+      case MQRC_NONE : break;             //
+      default: goto _door;                //
+    }                                     //
+                                          //
+    if( memcmp( qname, EMPTY_Q_NAME, MQ_Q_NAME_LENGTH) == 0 )
+    {                                     // ignore all queues with 
+      continue;                           //  empty remote queue manager name
+    }                                     //
+                                          //
+    mqrc = mqStrInq( qAttrBag          ,  // final
+                     MQCA_Q_NAME       ,  // get the remote queue name
+                     0                 ,  // 
+                     MQ_Q_NAME_LENGTH  ,  // 
+                     (PMQCHAR) alias[i],  // 
+                     &lng             );  // 
                                           //
     switch( mqrc )                        //  handle error
     {                                     //
@@ -186,9 +263,7 @@ const char** getQmgrAliases( MQHCONN hCon, int *sysRc )
     {                                     //
       goto _door;                         //
     }                                     //
-                                          //
-    
-  }
+  }                                       //
 
   _door:
 
